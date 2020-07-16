3a83e0a5b916063f91532d19c0cb20d8119bede1
Rohit Sharma <rohit231186@gmail.com>
2020-07-16 14:10:00 +0530

    Update lambda_function.py
    changed variable names and added a return object

--- lambda_function.py
+++ lambda_function.py
@@ -3,8 +3,8 @@ import boto3
 
 def lambda_handler(event, context):
     #searching for the faq details
-    client = boto3.client('cloudsearchdomain', endpoint_url='https://search-my-faq-domain-{unique-id}.us-east-1.cloudsearch.amazonaws.com')
-    response = client.search(
+    search_client = boto3.client('cloudsearchdomain', endpoint_url='https://search-my-faq-domain-{unique-id}.us-east-1.cloudsearch.amazonaws.com')
+    search_response = search_client.search(
         #query="matchall", #use this with the 'structured' query parser below to search for all documents
         #queryParser='structured',
         query=event["faq"],
@@ -13,16 +13,17 @@ def lambda_handler(event, context):
         sort='popularity desc'
     )
     #retrieving the document data
-    document = response["hits"]["hit"][0]
+    document = search_response["hits"]["hit"][0]
     document_id = document["id"]
     document_faq = document["fields"]["faq"][0]
     document_popularity = int(document["fields"]["popularity"][0]) + 1
     
     #updating the document
-    client1 = boto3.client('cloudsearchdomain', endpoint_url='https://doc-my-faq-domain-{unique-id}.us-east-1.cloudsearch.amazonaws.com')
+    update_client = boto3.client('cloudsearchdomain', endpoint_url='https://doc-my-faq-domain-{unique-id}.us-east-1.cloudsearch.amazonaws.com')
     upload_batch_json_string = "[{\"type\": \"add\", \"id\": \""+document_id+"\", \"fields\": {\"popularity\": \""+str(document_popularity)+"\", \"faq\": \""+document_faq+"\"}}]"
-    response1 = client1.upload_documents(
+    update_response = update_client.upload_documents(
        documents= bytes(upload_batch_json_string, 'utf-8'),
         #documents='data.json',  #use this if you want to upload a file
         contentType='application/json'
     )
+    return {"status": update_response["status"]}
